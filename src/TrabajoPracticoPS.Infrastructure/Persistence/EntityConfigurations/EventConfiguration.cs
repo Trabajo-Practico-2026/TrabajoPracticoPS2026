a418004725514b6943f6ad717dfa49bcf3f70134
Valeria Diaz <vale-peke-22@hotmail.com>
2026-04-18 23:20:59 -0300

    Base de datos Lista para usar

--- src/TrabajoPracticoPS.Infrastructure/Persistence/EntityConfigurations/EventConfiguration.cs
+++ src/TrabajoPracticoPS.Infrastructure/Persistence/EntityConfigurations/EventConfiguration.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TrabajoPracticoPS.Domain.Entities;
 
 namespace TrabajoPracticoPS.Infrastructure.Persistence.EntityConfigurations
